4c1063f317d056a865e86c5647a8f839ff9c6b73
hellowein <hellowein2@gmail.com>
2024-05-07 16:03:02 +0300

    update

--- main.py
+++ main.py
@@ -3,23 +3,40 @@ import sqlite3
 
 # Создаем подключение к базе данных
 connection = sqlite3.connect('data_users.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id TEXT NOT NULL,
+username TEXT NOT NULL
+)
+''')
+connection.commit()
 connection.close()
+
+
+
 
 API_TOKEN = '6962301596:AAGFJ9IUFDQL62-EcHUAvOBerCGaBsBSOiQ'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 
+def add_user_data(message):
+    connection = sqlite3.connect('data_users.db')
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO Users (username, id) VALUES (?, ?)', ('newuser', '@helloweim'))
+    print(message.from_user.username)
+    connection.commit()
+    connection.close()
+
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.reply_to(message, 'Ку я todo list v0.0.1')
+    add_user_data(message)
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     bot.reply_to(message, message.text)
