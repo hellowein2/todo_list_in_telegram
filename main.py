85eb10d5b8ab1f6190a93b1e57d08fd179d74046
hellowein <hellowein2@gmail.com>
2024-05-07 16:14:12 +0300

    update

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import telebot
 import sqlite3
 
-# Создаем подключение к базе данных
-connection = sqlite3.connect('data_users.db')
-cursor = connection.cursor()
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id TEXT NOT NULL,
-username TEXT NOT NULL
-)
-''')
-connection.commit()
-connection.close()
+# # Создаем подключение к базе данных
+# connection = sqlite3.connect('data_users.db')
+# cursor = connection.cursor()
+#
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS Users (
+# user_id INTEGER NOT NULL,
+# username TEXT NOT NULL
+# )
+# ''')
+# connection.commit()
+# connection.close()
 
 
 
@@ -23,23 +23,21 @@ bot = telebot.TeleBot(API_TOKEN)
 
 
 def add_user_data(message):
+    username = message.from_user.username
+    user_id = message.from_user.id
+
     connection = sqlite3.connect('data_users.db')
     cursor = connection.cursor()
-    cursor.execute('INSERT INTO Users (username, id) VALUES (?, ?)', ('newuser', '@helloweim'))
-    print(message.from_user.username)
+    cursor.execute('INSERT INTO Users (user_id, username) VALUES (?, ?)', (f'{user_id}', f'{username}'))
+
     connection.commit()
     connection.close()
 
-# Handle '/start' and '/help'
+
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, 'Ку я todo list v0.0.1')
     add_user_data(message)
-
-
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
 
 
 bot.infinity_polling()
