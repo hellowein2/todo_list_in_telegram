3b879b52d2f5b0f5bc7bf97bfa2fd71b31c106cf
hellowein <hellowein2@gmail.com>
2024-05-08 00:22:53 +0300

    users can view tasks

--- main.py
+++ main.py
@@ -56,11 +56,28 @@ def add_task(message):
     cursor.close()
     connection.close()
     bot.send_message(message.chat.id, 'Задача добавлена!')
+
+
+
+def view_tasks(message):
+    connection = sqlite3.connect('ignore/data_users.db')
+    cursor = connection.cursor()
+    task = ''
+    count = 0
+    for i in cursor.execute(f'SELECT * FROM Tasks{message.from_user.id}'):
+        count += 1
+        task += f'{count}: {i[0]} {i[1]}\n'
+    bot.send_message(message.chat.id, task)
+
+    cursor.close()
+    connection.close()
+
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton('Добавить задачу')
-    markup.row(btn1)
+    btn2 = types.KeyboardButton('Показать задачи')
+    markup.row(btn1, btn2)
     add_user_data(message)
     bot.send_message(message.chat.id, f'Ку {message.from_user.first_name} я todo list v0.0.1', reply_markup=markup)
 
@@ -70,6 +87,9 @@ def send_message(message):
     if message.text == 'Добавить задачу':
         msg = bot.send_message(message.chat.id, 'Введите задачу')
         bot.register_next_step_handler(msg, add_task)
+    if message.text == 'Показать задачи':
+        view_tasks(message)
+
 
 
 bot.infinity_polling()
