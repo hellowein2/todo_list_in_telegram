f924197392a49ecf30fc483e30f67aff7f2a4696
hellowein <hellowein2@gmail.com>
2024-05-08 02:05:17 +0300

    update

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def send_welcome(message):
     btn2 = types.KeyboardButton('Показать задачи')
     markup.row(btn1, btn2)
     add_user_data(message)
-    bot.send_message(message.chat.id, f'Ку {message.from_user.first_name} я todo list v0.0.1', reply_markup=markup)
+    bot.send_message(message.chat.id, f'Ку {message.from_user.first_name} я todo list v0.0.2', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
